fe78c39fa1046c3dd61c8f6fab1979afa800b1d4
AsteriskAmpersand <araveugnitsuga@gmail.com>
2020-01-21 18:02:56 -0500

    Added iceborne resource binding structure types
    Added code for generating the possible strings from the scr decompiled
    files.

---
+++ fetch_shader_classes/list_bindings.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 21 17:25:58 2020
+
+@author: AsteriskAmpersand
+"""
+
+from pathlib import Path
+
+def removeWhitespace(string):
+    return ' '.join(string.split())
+
+nameSet = set()
+nameList = []
+
+for filePath in Path(r"E:\MHW Shader Research\MHW-Research\shdr").rglob("*.src"):
+    file = filePath.open("r")
+    #print(filePath)
+    for line in file:
+        if "// Resource Bindings:" in line:
+            next(file)
+            header = next(file).replace("\r","").replace("\n","")
+            header = removeWhitespace(header).split(" ")
+            next(file)
+            for line in file:
+                if line.replace("\r","").replace("\n","") == "//":
+                    break
+                line = line.replace("\r","").replace("\n","")
+                comment, bname,btype,bformat,bdim,bslot,belements = removeWhitespace(line).split(" ")
+                if bname not in nameSet:
+                    nameSet.add(bname)
+                    nameList.append(bname)
+print(','.join(nameList))
